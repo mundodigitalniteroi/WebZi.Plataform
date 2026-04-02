07c350c9f5e39d4abe1d8f539faeb6d29ec88108
egotting <henricodss@gmail.com>
2026-04-02 12:21:36 -0300

    feat: ajuste no atualizar reboquista

---
+++ WebZi.Plataform.Domain/ViewModel/Reboquista/AtualizarReboquistaParameters.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebZi.Plataform.Domain.ViewModel.GRV.Cadastro
+{
+    public class AtualizarReboquistaParameters
+    {
+        [Required(ErrorMessage = "Propriedade obrigatória")]
+        public int IdentificadorReboquista { get; set; }
+
+        [Required(ErrorMessage = "Propriedade obrigatória")]
+        public int IdentificadorUsuario { get; set; }
+        [Required(ErrorMessage = "Propriedade obrigatória")]
+        public int IdentificadorCliente { get; set; }
+        [Required(ErrorMessage = "Propriedade obrigatória")]
+        public int IdentificadorDeposito { get; set; }
+        [Required(ErrorMessage = "Propriedade obrigatória")]
+        [StringLength(100, ErrorMessage = "O nome do reboquista deve conter no máximo 100 caracteres.")]
+        public string NomeReboquista { get; set; }
+        [StringLength(1, ErrorMessage = "A Flag de Ativo deve conter no máximo 1 caracteres.")]
+        public string FlagAtivo { get; set; } = "S";
+    }
+}
