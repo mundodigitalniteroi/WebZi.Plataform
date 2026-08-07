a918e00e88dd3bda13f59964e657d5fc1503ad67
egotting <henricodss@gmail.com>
2026-08-07 13:41:37 -0300

    feat: body para cadastro de usuario

---
+++ WebZi.Plataform.Domain/ViewModel/Usuario/CadastroUsuario/CadastroUsuarioParameters.cs
@@ -0,0 +1,15 @@
+﻿namespace WebZi.Plataform.Domain.ViewModel.Usuario.CadastroUsuario;
+
+public class CadastroUsuarioParameters
+{
+    public int identificadorUsuario { get; set; }
+    public string Login { get; set; }
+    public long identificadorPessoa { get; set; }
+    public string Matricula { get; set; }
+
+    public List<PermissoesUsuarioParameters> PermissoesUsuario { get; set; }
+
+    public List<int> PerfisDeAcesso { get; set; }
+    public List<int> VincularCliente { get; set; }
+    public List<int> VincularDeposito { get; set; }
+}
