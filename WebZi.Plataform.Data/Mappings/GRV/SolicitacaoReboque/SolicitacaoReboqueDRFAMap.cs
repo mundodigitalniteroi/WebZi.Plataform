33bf3871a46f2e7a0d92a2bd3caad4bf723c29cc
egotting <henricodss@gmail.com>
2026-08-28 13:20:26 -0300

    feat: SolicitacaoReboque para DRFA

---
+++ WebZi.Plataform.Data/Mappings/GRV/SolicitacaoReboque/SolicitacaoReboqueDRFAMap.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebZi.Plataform.Domain.Models.GRV.SolicitacaoReboque;
+
+namespace WebZi.Plataform.Data.Mappings.GRV.SolicitacaoReboque;
+
+public class SolicitacaoReboqueDRFAMap : IEntityTypeConfiguration<SolicitacaoReboqueDRFAModel>
+{
+    public void Configure(EntityTypeBuilder<SolicitacaoReboqueDRFAModel> builder)
+    {
+        builder.ToTable("tb_dep_solicitacao_reboque_drfa").HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("id_solicitacao_reboque_drfa")
+            .ValueGeneratedOnAdd();
+
+        builder.Property(x => x.SolicitacaoReboqueId)
+            .HasColumnName("id_solicitacao_reboque")
+            .IsRequired();
+
+        builder.Property(x => x.TipoRegistroId)
+            .HasColumnName("id_grv_drfa_tipo_registro")
+            .HasColumnType("tinyint")
+            .IsRequired();
+
+        builder.Property(x => x.OrgaoEmissorId)
+            .HasColumnName("id_orgao_emissor")
+            .HasColumnType("smallint");
+
+        builder.Property(x => x.AutoridadeDivisaoId)
+            .HasColumnName("id_autoridade_divisao")
+            .HasColumnType("tinyint");
+
+        builder.Property(x => x.AutoridadeDivisaoComplemento)
+            .HasColumnName("autoridade_divisao_complemento")
+            .HasMaxLength(15)
+            .IsUnicode(false);
+
+        builder.Property(x => x.NumeroRegistroRouboFurto)
+            .HasColumnName("numero_registro_roubo_furto")
+            .HasMaxLength(35)
+            .IsUnicode(false);
+
+        builder.Property(x => x.RegistroRouboFurtoMatriculaAgente)
+            .HasColumnName("registro_roubo_furto_matricula_agente")
+            .HasMaxLength(35)
+            .IsUnicode(false);
+
+        builder.Property(x => x.RegistroRouboFurtoNomeAgente)
+            .HasColumnName("registro_roubo_furto_nome_agente")
+            .HasMaxLength(100)
+            .IsUnicode(false);
+
+        builder.Property(x => x.EstadoGeralVeiculo)
+            .HasColumnName("estado_geral_veiculo")
+            .HasMaxLength(500)
+            .IsUnicode(false);
+
+        builder.HasOne(x => x.SolicitacaoReboque)
+            .WithMany()
+            .HasForeignKey(x => x.SolicitacaoReboqueId);
+
+        builder.HasOne(x => x.TipoRegistro)
+            .WithMany()
+            .HasForeignKey(x => x.TipoRegistroId);
+
+        builder.HasOne(x => x.OrgaoEmissor)
+            .WithMany()
+            .HasForeignKey(x => x.OrgaoEmissorId);
+
+        builder.HasOne(x => x.AutoridadeDivisao)
+            .WithMany()
+            .HasForeignKey(x => x.AutoridadeDivisaoId);
+    }
+}
---
+++ WebZi.Plataform.Domain/Models/GRV/SolicitacaoReboque/SolicitacaoReboqueDRFAModel.cs
@@ -0,0 +1,21 @@
+﻿using WebZi.Plataform.Domain.Models.Documento;
+
+namespace WebZi.Plataform.Domain.Models.GRV.SolicitacaoReboque;
+
+public class SolicitacaoReboqueDRFAModel
+{
+    public int Id { get; set; }
+    public int SolicitacaoReboqueId { get; set; }
+    public byte TipoRegistroId { get; set; }
+    public short? OrgaoEmissorId { get; set; }
+    public byte? AutoridadeDivisaoId { get; set; }
+    public string? AutoridadeDivisaoComplemento { get; set; }
+    public string? NumeroRegistroRouboFurto { get; set; }
+    public string? RegistroRouboFurtoMatriculaAgente { get; set; }
+    public string? RegistroRouboFurtoNomeAgente { get; set; }
+    public string? EstadoGeralVeiculo { get; set; }
+    public SolicitacaoReboqueModel SolicitacaoReboque { get; set; }
+    public TipoRegistroModel TipoRegistro { get; set; }
+    public OrgaoEmissorModel? OrgaoEmissor { get; set; }
+    public AutoridadeDivisaoModel? AutoridadeDivisao { get; set; }
+}
